500e98554dbb0333021bdb699357e46a326adefe
dstephens179 <david.stephens365@gmail.com>
2022-07-01 08:55:14 -0400

    housekeeping on file.

--- forecast_upload_to_bigquery.R
+++ forecast_upload_to_bigquery.R
@@ -5,19 +5,25 @@ library(bigrquery)
 datasetid <- "source-data-314320.joyeria_dataset.forecast"
 
 
+# Append all forecasted data
+tidy_fcast <- bind_rows(tidy_Centro, 
+                        tidy_Segovia, 
+                        tidy_Patria, 
+                        tidy_Lupe,
+                        tidy_Vallardo)
 
-# bind_rows is great. This let's you append the tidy_data created in each store's forecast
-tidy_fcast <- bind_rows(tidy_Centro, tidy_Segovia, tidy_Patria, tidy_Lupe, tidy_Vallardo)
 
 # using which() to select only "forecast" rows in the key column
 tidy_subset <- tidy_fcast[which(tidy_fcast$key == "forecast"), names(tidy_fcast)]
 
-# increase subset forecast by +0%.
-# This is global. If you want to change on the store-level, you will have to create it.
+
+
+# Global forecast increase by x%
 tidy_subset$total_sales <- tidy_subset$total_sales*1.00
 
 
-# use bigrquery to upload/overwrite the dataset
+
+# bigrquery to upload & overwrite the dataset
 bq_perform_upload(datasetid,
                   tidy_subset,
                   nskip = 0,
